9ff1faa14fa4d2b62f77b7b2f18d983c70a19562
Marisha Sahay <marishasahay@gmail.com>
2016-09-12 21:18:05 -0700

    work in progress

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -3,4 +3,13 @@ class ResumesController < ApplicationController
     @students = Student.all
     render json: @students
   end
+
+  def create 
+    @student = Student.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number], short_bio: params[:short_bio], linkedin: params[:linkedin], twitter_handle: params[:twitter_handle], website: params[:website], resume: params[:resume], github: params[:github], photo: params[:photo], experiences: params[:experiences])
+    if @student.save
+      render :show
+    else
+      render json: {errors: @student.errors.full_messages}
+    end
+  end
 end
