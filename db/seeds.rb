7c8e3137716d21ee6a684c402fd7137ef95c2781
Steen Clarke <cmclarkey@gmail.com>
2016-09-12 20:55:06 -0700

    Install seed dump, dumped seed

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Education.create!([
+  {degree: "Master", university_name: "School of hard knocks", details: "Got some book learning", start_date: "1985-01-01", end_date: "2000-01-01", student_id: 1}
+])
+Experience.create!([
+  {job_title: "Captain", company_name: "Good Ship Tarman", details: "Captained the liveship Tarman on the Rain Wild River", start_date: "2001-01-01", end_date: "2008-01-01", student_id: 1}
+])
+Student.create!([
+  {first_name: "Bill", last_name: "Will", email: "bill@will.com", phone_number: "(415) 690-1234", short_bio: "Bill will is a good man, a man of the earth, and like the earth he is a solid foundation", linkedin: "linkedin.com/bill-will", twitter_handle: "@bill_will", website: "www.bill-will.com", resume: "www.bill-will.com/resume", github: "www.github.com/bill-will", photo: "www.bill-will.com/photo.jpg"}
+])
