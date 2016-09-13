99db6303cd837973c7d4ddbb42d24868c65dc746
Steen Clarke <cmclarkey@gmail.com>
2016-09-12 21:15:13 -0700

    Make strong params for student

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,4 +1,5 @@
 class ResumesController < ApplicationController
+
   def index
     @students = Student.all
     render :json => @students, :include => [
@@ -8,4 +9,56 @@ class ResumesController < ApplicationController
       :skills
     ]
   end
+
+  def new
+  end
+
+  def create
+    @student = Student.new(student_params)
+  end
+
+  def edit
+    @student = Student.find_by(id: params[:id])
+  end
+
+  def update
+  end
+
+  private
+
+    def student_params
+      params.require(:student).permit(
+        :first_name,
+        :last_name,
+        :email,
+        :phone_number,
+        :short_bio,
+        :linkedin,
+        :twitter_handle,
+        :website,
+        :resume,
+        :github,
+        :photo
+        educations_attributes: [
+          :id, 
+          :degree, 
+          :university_name, 
+          :details, 
+          :start_date,
+          :end_date,
+          :student_id,
+          :_destroy
+        ],
+        experiences_attributes: [
+          :id,
+          :job_title,
+          :company_name,
+          :details,
+          :start_date,
+          :end_date,
+          :student_id
+        ]
+        )
+    end
+
 end
