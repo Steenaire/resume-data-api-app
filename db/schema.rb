4745cff8ed55e970d353644c921c47a9bac7cca9
Steen Clarke <cmclarkey@gmail.com>
2016-09-14 18:21:48 -0700

    make model for users, to store api keys and emails for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913030922) do
+ActiveRecord::Schema.define(version: 20160915011820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(version: 20160913030922) do
     t.string   "photo"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "api_key"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
