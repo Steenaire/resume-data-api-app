7584a6513ba6a7fbb7503e75fe1580f1186879cd
Marisha Sahay <marishasahay@gmail.com>
2016-09-14 18:50:51 -0700

    Add one more record

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,13 @@ Experience.create!([
 ])
 Student.create!([
   {first_name: "Bill", last_name: "Will", email: "bill@will.com", phone_number: "(415) 690-1234", short_bio: "Bill will is a good man, a man of the earth, and like the earth he is a solid foundation", linkedin: "linkedin.com/bill-will", twitter_handle: "@bill_will", website: "www.bill-will.com", resume: "www.bill-will.com/resume", github: "www.github.com/bill-will", photo: "www.bill-will.com/photo.jpg"}
+]),
+Education.create!([
+  {degree: "Bachelors", university_name: "School of Management", details: "See website for details.", start_date: "1905-12-12", end_date: "2050-01-01", student_id: 2}
+])
+Experience.create!([
+  {job_title: "Software engineer", company_name: "Happy employees", details: "Have been involved in development of many of the modules specially payment", start_date: "2008-09-03", end_date: "2013-01-01", student_id: 2}
+])
+Student.create!([
+  {first_name: "Bob", last_name: "Miller", email: "bob@acltc.com", phone_number: "(415) 673-4321", short_bio: "Bob is a good and hardworking employee. He is a good team worker. He helps everyone", linkedin: "linkedin.com/bob-123456", twitter_handle: "@bobmiller", website: "www.bob.com", resume: "www.bob-purple-hippo.com/resume", github: "www.github.com/bob-miller", photo: "http://www.sterling-wellness.com/wp-content/uploads/2012/10/Client-Testimonials-e1351276434178.jpg"}
 ])
